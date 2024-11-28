eb41d3115b9564789a1a0e6166d3b39881f199b9
Callie Cameron <cjcameron7@gmail.com>
2024-11-28 13:59:42 +0100

    Test new package creation in the container.

--- tests/container/setup_tests.sh
+++ tests/container/setup_tests.sh
@@ -2,15 +2,14 @@
 
 set -eu
 
-THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
 export GIT_AUTHOR_NAME='test'
 export GIT_COMMITTER_NAME='test'
 export GIT_AUTHOR_EMAIL='test@example.com'
 export GIT_COMMITTER_EMAIL='test@example.com'
 
 # Main repo
-cd "${THIS_DIR}/rules_markdown"
+TEST_DIR="${HOME}/rules_markdown"
+cd "${TEST_DIR}"
 git -c init.defaultBranch=main init .
 git add --all
 git commit --quiet -m 'Test'
@@ -18,24 +17,64 @@ bazel run :git_update
 bazel clean
 
 # Unversioned copy
-cd "${THIS_DIR}"
-cp -a rules_markdown/tests/other_workspace other-workspace-unversioned
-rm other-workspace-unversioned/.gitignore
-sed -i 's|path = "../.."|path = "../rules_markdown"|g' other-workspace-unversioned/MODULE.bazel
-sed -i 's|versioned_test|unversioned_test|g' other-workspace-unversioned/tests/test6/BUILD
+TEST_DIR="${HOME}/other-workspace-unversioned"
+cp -a "${HOME}/rules_markdown/tests/other_workspace" "${TEST_DIR}"
+rm "${TEST_DIR}/.gitignore"
+sed -i 's|path = "../.."|path = "../rules_markdown"|g' "${TEST_DIR}/MODULE.bazel"
+sed -i 's|versioned_test|unversioned_test|g' "${TEST_DIR}/tests/test6/BUILD"
+mkdir "${TEST_DIR}/new"
+cd "${TEST_DIR}/new"
+bazel run //:new
+printf "\nTest text\n" >>'new.md'
+buildozer 'new_load @rules_markdown//markdown/testing:defs.bzl output_test' '//new:all'
+buildozer 'new_load @rules_markdown//markdown/testing:defs.bzl versioned_test' '//new:all'
+cat <<EOF >>BUILD
 
-# Versioned copy
-cd "${THIS_DIR}"
-cp -a rules_markdown/tests/other_workspace other-workspace-versioned
-rm other-workspace-versioned/.gitignore
-sed -i 's|path = "../.."|path = "../rules_markdown"|g' other-workspace-versioned/MODULE.bazel
-cd other-workspace-versioned
-buildozer "new_load @markdown//:defs.bzl md_git_repo" "//:all"
-printf '\nmd_git_repo()\n' >>BUILD
-git -c init.defaultBranch=main init .
+output_test(
+    reproducible = False,
+    target = "new",
+)
+
+versioned_test(
+    target = "new",
+)
+EOF
 bazel run :git_update
+"${TEST_DIR}/.markdown_summary/refresh"
 bazel clean
 git add --all
 git commit --no-verify --quiet -m 'Test'
 
-rm -rf ~/.cache/bazel
+# Versioned copy
+TEST_DIR="${HOME}/other-workspace-versioned"
+cp -a "${HOME}/rules_markdown/tests/other_workspace" "${TEST_DIR}"
+rm "${TEST_DIR}/.gitignore"
+sed -i 's|path = "../.."|path = "../rules_markdown"|g' "${TEST_DIR}/MODULE.bazel"
+cd "${TEST_DIR}"
+buildozer 'new_load @markdown//:defs.bzl md_git_repo' '//:all'
+printf '\nmd_git_repo()\n' >>BUILD
+git -c init.defaultBranch=main init .
+bazel run :git_update
+mkdir "${TEST_DIR}/new"
+cd "${TEST_DIR}/new"
+bazel run //:new
+printf "\nTest text\n" >>'new.md'
+buildozer 'new_load @rules_markdown//markdown/testing:defs.bzl output_test' '//new:all'
+buildozer 'new_load @rules_markdown//markdown/testing:defs.bzl versioned_test' '//new:all'
+cat <<EOF >>BUILD
+
+output_test(
+    reproducible = False,
+    target = "new",
+)
+
+versioned_test(
+    target = "new",
+)
+EOF
+"${TEST_DIR}/.markdown_summary/refresh"
+bazel clean
+git add --all
+git commit --no-verify --quiet -m 'Test'
+
+rm -rf "${HOME}/.cache/bazel"
