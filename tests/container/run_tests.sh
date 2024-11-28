a068043541b600cafb26a095feddd40f74cdb2ec
Callie Cameron <cjcameron7@gmail.com>
2024-11-28 17:37:13 +0100

    Test new workspace creation in the container.

--- tests/container/run_tests.sh
+++ tests/container/run_tests.sh
@@ -5,6 +5,16 @@ set -eu
 THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "${THIS_DIR}"
 
-./rules_markdown/markdown/private/git/run_tests rules_markdown
-./rules_markdown/markdown/private/git/run_tests other-workspace-unversioned
-./rules_markdown/markdown/private/git/run_tests other-workspace-versioned
+RUN_TESTS="${HOME}/rules_markdown/markdown/private/git/run_tests"
+
+"${RUN_TESTS}" "${HOME}/rules_markdown"
+"${RUN_TESTS}" "${HOME}/other-workspace-unversioned"
+"${RUN_TESTS}" "${HOME}/other-workspace-versioned"
+
+# We have to build first so that MODULE.bazel.lock doesn't change during the
+# test run
+(
+    cd "${HOME}/new-workspace"
+    bazel build ...:all
+)
+"${RUN_TESTS}" "${HOME}/new-workspace"
--- tests/container/setup_tests.sh
+++ tests/container/setup_tests.sh
@@ -2,15 +2,47 @@
 
 set -eu
 
-export GIT_AUTHOR_NAME='test'
-export GIT_COMMITTER_NAME='test'
-export GIT_AUTHOR_EMAIL='test@example.com'
-export GIT_COMMITTER_EMAIL='test@example.com'
+function usage() {
+    echo "Usage: $(basename "${0}") bazel_version"
+    exit 1
+}
+
+test -z "${1:-}" && usage
+BAZEL_VERSION="${1}"
+
+git config --global user.name 'test'
+git config --global user.email 'test@example.com'
+git config --global init.defaultBranch 'main'
+
+function new-package() {
+    mkdir "${TEST_DIR}/newpackage"
+    cd "${TEST_DIR}/newpackage"
+    bazel run //:new
+    printf "\nTest text\n" >>'newpackage.md'
+    echo 'newpackage' >'newpackage.dic'
+    buildozer 'new_load @rules_markdown//markdown/testing:defs.bzl output_test' '//newpackage:all'
+    buildozer 'new_load @rules_markdown//markdown/testing:defs.bzl versioned_test' '//newpackage:all'
+    cat <<EOF >>BUILD
+
+output_test(
+    reproducible = False,
+    target = "newpackage",
+)
+
+versioned_test(
+    target = "newpackage",
+)
+EOF
+    if grep md_git_repo BUILD >/dev/null; then
+        bazel run :git_update
+    fi
+    "${TEST_DIR}/.markdown_summary/refresh"
+}
 
 # Main repo
 TEST_DIR="${HOME}/rules_markdown"
 cd "${TEST_DIR}"
-git -c init.defaultBranch=main init .
+git init .
 git add --all
 git commit --quiet -m 'Test'
 bazel run :git_update
@@ -22,25 +54,7 @@ cp -a "${HOME}/rules_markdown/tests/other_workspace" "${TEST_DIR}"
 rm "${TEST_DIR}/.gitignore"
 sed -i 's|path = "../.."|path = "../rules_markdown"|g' "${TEST_DIR}/MODULE.bazel"
 sed -i 's|versioned_test|unversioned_test|g' "${TEST_DIR}/tests/test6/BUILD"
-mkdir "${TEST_DIR}/new"
-cd "${TEST_DIR}/new"
-bazel run //:new
-printf "\nTest text\n" >>'new.md'
-buildozer 'new_load @rules_markdown//markdown/testing:defs.bzl output_test' '//new:all'
-buildozer 'new_load @rules_markdown//markdown/testing:defs.bzl versioned_test' '//new:all'
-cat <<EOF >>BUILD
-
-output_test(
-    reproducible = False,
-    target = "new",
-)
-
-versioned_test(
-    target = "new",
-)
-EOF
-bazel run :git_update
-"${TEST_DIR}/.markdown_summary/refresh"
+new-package
 bazel clean
 git add --all
 git commit --no-verify --quiet -m 'Test'
@@ -53,26 +67,30 @@ sed -i 's|path = "../.."|path = "../rules_markdown"|g' "${TEST_DIR}/MODULE.bazel
 cd "${TEST_DIR}"
 buildozer 'new_load @markdown//:defs.bzl md_git_repo' '//:all'
 printf '\nmd_git_repo()\n' >>BUILD
-git -c init.defaultBranch=main init .
+git init .
 bazel run :git_update
-mkdir "${TEST_DIR}/new"
-cd "${TEST_DIR}/new"
-bazel run //:new
-printf "\nTest text\n" >>'new.md'
-buildozer 'new_load @rules_markdown//markdown/testing:defs.bzl output_test' '//new:all'
-buildozer 'new_load @rules_markdown//markdown/testing:defs.bzl versioned_test' '//new:all'
-cat <<EOF >>BUILD
+new-package
+bazel clean
+git add --all
+git commit --no-verify --quiet -m 'Test'
 
-output_test(
-    reproducible = False,
-    target = "new",
-)
-
-versioned_test(
-    target = "new",
-)
-EOF
-"${TEST_DIR}/.markdown_summary/refresh"
+# New workspace
+TEST_DIR="${HOME}/new-workspace"
+mkdir "${TEST_DIR}"
+cp "${HOME}/rules_markdown/markdown/private/workspace/default_bazeliskrc" "${TEST_DIR}/.bazeliskrc"
+cp "${HOME}/rules_markdown/markdown/private/workspace/default_bazelrc" "${TEST_DIR}/.bazelrc"
+touch "${TEST_DIR}/WORKSPACE"
+sed "s/@@@@@/${BAZEL_VERSION}/g" <"${HOME}/rules_markdown/readme/module_template.bazel" |
+    tr '\n' '\t' |
+    perl -pe 's|archive_override\([^\)]*?\)|local_path_override(\t    module_name = "rules_markdown",\t    path = "../rules_markdown",\t)|g' |
+    tr '\t' '\n' >"${TEST_DIR}/MODULE.bazel"
+cp "${HOME}/rules_markdown/readme/root.build" "${TEST_DIR}/BUILD"
+cd "${TEST_DIR}"
+git init .
+bazel run :workspace_update
+bazel run :contents_update
+bazel run :git_update
+new-package
 bazel clean
 git add --all
 git commit --no-verify --quiet -m 'Test'
